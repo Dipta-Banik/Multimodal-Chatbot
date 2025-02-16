555cfc32a3ed42a0717e712f476725e78d63ce7b
Dipta Banik <107272126+Dipta-Banik@users.noreply.github.com>
2025-02-16 18:51:25 +0530

    Update summary.py
    line no 11 punkt_tab

--- summary.py
+++ summary.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import spacy
 import heapq
 
-nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
